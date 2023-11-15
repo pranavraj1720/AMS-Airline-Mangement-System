94b882ded2d55a186ac68b619813f007b61756f2
Pranav Raj <pranavraj1720@gmail.com>
2023-11-15 18:42:15 +0530

    Created SIGN UP AND LOGIN Validation Logic

--- login.py
+++ login.py
@@ -1,13 +1,19 @@
 import customtkinter as ctk
+from screeninfo import get_monitors
+import re
+
+
 root = ctk.CTk()
-
-
 # Setting appearance mode and color theme 
 ctk.set_appearance_mode("light")
 ctk.set_default_color_theme("blue") 
 
+resoultion = get_monitors()[0]
+screenWidth = resoultion.width
+screenHeight = resoultion.height
 
-root.geometry("1366x768")
+
+root.geometry(f"{screenWidth}x{screenHeight}")
 root.title("Log In")
 
 
@@ -20,6 +26,20 @@ loginFrame = ctk.CTkFrame(master=root, corner_radius=8, border_width=1, border_c
 loginFrame.pack(pady=20)
 
 
+def validateLogIn():
+    username = userEntry.get()
+    password = passEntry.get()
+    if not username:
+        error_label.configure(text="Please enter a username", text_color="red", font=('inter', 23))
+        error_label.pack(pady = (20, 0))
+    elif not password:
+        error_label.configure(text="Please enter a password", text_color="red", font=('inter', 23))
+        error_label.pack(pady = (20, 0))
+    else:
+        error_label.configure(text="")
+        error_label.grid_forget()
+        
+
 # Creating userentry and passentry for username and password
 userEntry = ctk.CTkEntry(master=loginFrame ,placeholder_text="username", width=400, font=('calibri', 25), corner_radius=10, border_color="grey", fg_color="white", border_width=2)
 userEntry.pack(pady=28, padx=10)
@@ -28,9 +48,13 @@ passEntry = ctk.CTkEntry(master=loginFrame, placeholder_text="password", show="*
 passEntry.pack(pady=0, padx=10)
 
 
+
+error_label = ctk.CTkLabel(loginFrame, text="")
+error_label.pack()
+
 # login button
 button = ctk.CTkButton(master=loginFrame, 
-                       text='Login', font=('inter', 25), fg_color="grey") 
+                       text='Login', font=('inter', 25), fg_color="grey", command=validateLogIn) 
 button.pack(pady=20,padx=200) 
   
 #  Forget password and create new account
--- signup.py
+++ signup.py
@@ -1,13 +1,18 @@
 import customtkinter as ctk
 from screeninfo import get_monitors
 import re
+import subprocess
+import sys  
+
+
+
 
 root = ctk.CTk()
 resoultion = get_monitors()[0]
 screenWidth = resoultion.width
 screenHeight = resoultion.height
 
-print(screenWidth, screenHeight)
+
 
 
 # Setting appearance mode and color theme
@@ -15,7 +20,7 @@ ctk.set_appearance_mode("light")
 ctk.set_default_color_theme("blue")
 
 root.geometry(f"{screenWidth}x{screenHeight}")
-root.title("Log In")
+root.title("Create Account")
 
 # Empty space to center content horizontally
 root.grid_rowconfigure(0, weight=1)
@@ -29,11 +34,12 @@ logIn.grid(row=1, column=1, pady=50)
 
 # Creating login frame with more height
 SingupFrame = ctk.CTkFrame(master=root, corner_radius=8, border_width=1, border_color="black", fg_color="white")
-SingupFrame.grid(row=2, column=1, pady=(20, 30))  # Increase pady to create more height
+SingupFrame.grid(row=2, column=1, pady=(20, 30))  
 
 # Creating userentry and passentry for username and password
 userEntry = ctk.CTkEntry(master=SingupFrame, placeholder_text="username", width=400, font=('calibri', 25), corner_radius=10, border_color="grey", fg_color="white", border_width=2)
 userEntry.grid(row=0, column=0, pady=(30,15), padx=10)
+
 
 def is_valid_email(email):
     email_pattern = r'^[\w\.-]+@[\w\.-]+\.\w+$'
@@ -72,34 +78,36 @@ def validate_signupForm():
     }
 
 
-    print(all_entries)
+    
 
     for key, value in all_entries.items():
         if value == "":
-            error_label.configure(text="Please Add Items", text_color="red")
+            error_label.configure(text="Please Add Items", text_color="red", font=('inter', 18))
+            error_label.grid(row=6, column=0, columnspan=2, pady=(0, 30))
             return
     
-    print(contact.isdigit())
-    print(len(contact))
+    
 
 
     if not username:
-        error_label.configure(text="Please enter a username", text_color="red")
-        error_label.grid(row=6, column=0, columnspan=2, pady=20)
+        error_label.configure(text="Please enter a username", text_color="red", font=('inter', 18))
+        error_label.grid(row=6, column=0, columnspan=2, pady=(0, 30))
     elif not is_valid_email(email):
-        error_label.configure(text="Invalid email format", text_color="red")
-        error_label.grid(row=6, column=0, columnspan=2, pady=20)
+        error_label.configure(SingupFrame,  text="Invalid email format", text_color="red", font=('inter', 18))
+        error_label.grid(row=6, column=0, columnspan=2, pady=(0, 30))
     elif not age.isdigit() or int(age) >= 100:
-        error_label.configure(text="Enter a valid age (must be a number and less than 100)", text_color="red")
-        error_label.grid(row=6, column=0, columnspan=2, pady=20)
-    elif not contact.isdigit():
-        print("inside contact check")
-        error_label.configure(text="Contact number must be a number", text_color="red")
-        error_label.grid(row=6, column=0, columnspan=2, pady=20)
+        error_label.configure(SingupFrame,  text="Enter a valid age (must be a number and less than 100)", text_color="red", font=('inter', 18))
+        error_label.grid(row=6, column=0, columnspan=2, pady=(0, 30))
+    elif not (contact.isdigit() and len(contact) == 10):
+        error_label.configure(SingupFrame,  text="Contact number must be a number (must be a 10-digit-no)", text_color="red", font=('inter', 18))
+        error_label.grid(row=6, column=0, columnspan=2, pady=(0, 30))
     else:
         # All fields are valid, clear the error message and proceed with signup
         error_label.configure(text="")
         error_label.grid_forget()
+    
+
+
 
 
 ageEntry = ctk.CTkEntry(master=SingupFrame, placeholder_text="age", width=400, font=('calibri', 25), corner_radius=10, border_color="grey", fg_color="white", border_width=2)
@@ -112,9 +120,10 @@ contactEntry.grid(row=3, column=0, pady=(10,15), padx=10)
 button = ctk.CTkButton(master=SingupFrame, text='Sign Up', font=('inter', 23), fg_color="grey", command=validate_signupForm)
 button.grid(row=4, column=0, pady=20, padx=200)
 
+
 # Forget password and create a new account
 createAccount = ctk.CTkLabel(master=SingupFrame, text="Already have an account, Login Here", font=('Inter', 20))
-createAccount.grid(row=5, column=0, pady=(0, 30), padx=10)
+createAccount.grid(row=5, column=0, pady=(0, 20), padx=10)
 
 # Age Error Label
 error_label = ctk.CTkLabel(SingupFrame, text="", text_color="red",  font=('Inter', 15))
